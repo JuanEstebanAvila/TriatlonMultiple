0cc5d8554d2697aa0cb0d41c0639dafd7c0625c0
Esteban <atjuan.vina7@gmail.com>
2026-06-01 23:31:07 -0500

    Falta el service

--- Backend/Competidor/src/main/java/com/edu/udistrital/backend/competidor/config/Configuraciones.java
+++ Backend/Competidor/src/main/java/com/edu/udistrital/backend/competidor/config/Configuraciones.java
@@ -1,6 +1,7 @@
 package com.edu.udistrital.backend.competidor.config;
 
 import com.edu.udistrital.backend.competidor.service.ServiceCompetidor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,9 +30,27 @@ public class Configuraciones {
         };
     }
 
+    /**
+     * Configuración del Modelmapper
+     * @return
+     */
     @Bean
-    public void ModelMapper modelmaper(){
+    public ModelMapper modelMapperBean(){
         return new ModelMapper();
+    }
+
+    //Para cumplir con el Open/Close de la clase, la URL de la API "competidor", necesaria para algunos microservicios,
+    //se extrae y guarda en una variable
+    @Value("${api.url.carrera}")//Proviene del archivo "application.propierties" de la carpeta "resoruces"
+    private String urlApiCarrera;
+
+    /**
+     * Configuración Bean de WebClient, para poder comunicarse con las otras APIS
+     * @return
+     */
+    @Bean
+    public WebClient webClientBean(){
+        return WebClient.builder().baseUrl(urlApiCarrera).build(); //Se usa la variable que tiene el contenido en el properties
     }
 
 }
--- Backend/Competidor/src/main/java/com/edu/udistrital/backend/competidor/model/CompetidorDTO.java
+++ Backend/Competidor/src/main/java/com/edu/udistrital/backend/competidor/model/CompetidorDTO.java
@@ -1,4 +1,53 @@
 package com.edu.udistrital.backend.competidor.model;
 
-public class CompetidorDTO {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity //Clase que representa la entidad "Competidor"
+@Data
+@EqualsAndHashCode(callSuper = true) //Incluye los campos del padre "Persona"
+@NoArgsConstructor
+@Table(name = "competidores")
+public class CompetidorDTO extends Persona{
+
+    @Id //Llave primaría de la base de datos
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Autoincremental
+    private int id;
+
+    //La URL de la foto
+    @NotBlank(message = "La foto no puede estar vacía")
+    @Column(name = "foto", length = 120, nullable = false)
+    private String foto;
+
+    //Si participa o no en la modalidad cross
+    @NotNull(message = "Seleccione si participa o no en modalidad cross")
+    @Column(name = "modalidad_cross", nullable = false)
+    private Boolean modalidadCross;
+
+    //La categoría a la cual pertenece el competidor
+    @NotBlank(message = "Debe pertenecer a alguna categoria")
+    @Column(name = "categoria", lenght = 25, nullable = false)
+    private String categoria;
+
+    //La especialidad del competidor en el triatlón
+    @NotBlank(message = "Seleccione su especialidad en el triatlón")
+    @Column(name = "especialidad", nullable = false, length = 30)
+    private String especialidad;
+
+    @Column(name = "carrera")
+    private String carrera;
+
+    //Constructor que no tiene la llave primaria "id"
+    public CompetidorDTO(String nombre, int edad, String genero, int identificacion, String correo, String foto, Boolean modalidadCross, String categoria, String especialidad, String carrera) {
+        super(nombre, edad, genero, identificacion, correo);
+        this.foto = foto;
+        this.modalidadCross = modalidadCross;
+        this.categoria = categoria;
+        this.especialidad = especialidad;
+        this.carrera = carrera;
+    }
 }
